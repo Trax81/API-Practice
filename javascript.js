66ac384da5f7f11b33494b8e1b6a41732f98bf81
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-01-28 14:12:04 -0800

    Travis

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=11a85b25
 
 async function main() {
-    const response = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=11a85b25")
+    const response = await fetch(`http://www.omdbapi.com/?s=batman&apikey=11a85b25`)
+    
     const data = await response.json()
-    console.log(data)
-    emailRef.innerHTML = data.email
+   console.log(data);
+    const searchInput = document.querySelector(".search-input");
+    const searchValue = searchInput.value;
+
+const response = await fetch(
+  `http://www.omdbapi.com/?s=${searchValue}&apikey=11a85b25`
+);
 }
 main();
