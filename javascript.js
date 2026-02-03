a937c0c7454e7cb0034cc35a9dd9fa0da24989d6
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-02-02 16:10:52 -0800

    Travis

--- javascript.js
+++ javascript.js
@@ -4,6 +4,27 @@ const input = document.querySelector(".search-input");
 const button = document.querySelector(".search-btn");
 const resultsContainer = document.querySelector(".results");
 
-button.addEventListener("click", () => {
-  alert("Button clicked");
+button.addEventListener("click", async () => {
+  const searchValue = input.value;
+
+  const response = await fetch(
+    `http://www.omdbapi.com/?s=${searchValue}&apikey=11a85b25`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+});
+
+resultsContainer.innerHTML = "";
+
+data.Search.forEach(movie => {
+  resultsContainer.innerHTML += `
+    <div >
+      <img src="${movie.Poster}" alt="${movie.Title}" />
+      <h4>${movie.Title}</h4>
+      <h5>Year: <span>${movie.Year}</span></h5>
+      <h5>Type: <span>${movie.Type}</span></h5>
+    </div>
+  `;
 });
